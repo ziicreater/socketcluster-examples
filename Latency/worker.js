b4696f3bc1e83378390471d81dcedad2327fff37
Kevin Boon <kevin_boon@outlook.com>
2019-12-31 17:05:36 -0500

    Added Latency Example

---
+++ Latency/worker.js
@@ -0,0 +1,42 @@
+var SCWorker = require('socketcluster/scworker');
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var morgan = require('morgan');
+var healthChecker = require('sc-framework-health-check');
+
+class Worker extends SCWorker {
+  run() {
+    console.log('   >> Worker PID:', process.pid);
+    var environment = this.options.environment;
+
+    var app = express();
+
+    var httpServer = this.httpServer;
+    var scServer = this.scServer;
+
+    if (environment === 'dev') {
+      // Log every HTTP request.
+      // See https://github.com/expressjs/morgan for other available formats.
+      app.use(morgan('dev'));
+    }
+    app.use(serveStatic(path.resolve(__dirname, 'public')));
+
+    // Listen for HTTP GET "/health-check".
+    healthChecker.attach(this, app);
+
+    httpServer.on('request', app);
+
+    // Handle incoming websocket connections and listen for events.
+    scServer.on('connection', function (socket) {
+
+      /** Latency Function */
+      socket.on('latency', function (data, res) {
+        res(null, '');
+      });
+      
+    });
+  }
+}
+
+new Worker();
